faf31f699243bb1d5ccbdf6b396b66172e8081c0
Beckh24469 <79584489+Beckh24469@users.noreply.github.com>
2023-12-15 14:38:38 -0500

    New update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three';
 
 //starting position of the images from the top
-const STARTY = 0;
+const STARTY = -15;
 
 //create a new scene
 const scene = new THREE.Scene();
 
 //create and position the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = STARTY;
-camera.position.z = 30;
+camera.position.y = STARTY
+camera.position.z = 50;
 
 //Create list of images in the img folder 
 let imgList = [
@@ -17,13 +17,17 @@ let imgList = [
     'OSHA.png',
     'compTIA.png',
     'classpic.png',
-
+    'UnitreeGo1-1.png',
+    'UnitreeGo1-2.png',
+    'UnitreeGo1-3.png',
+    'FORMBAR_TEACHER.png',
+    'Response.png',
 ]
 
 //Add every listed image as a plane mesh with texture to scene
 for (const image in imgList) {
     //Every mesh has a geometry texture and material
-    const geometry = new THREE.PlaneGeometry(15, 18);
+    const geometry = new THREE.PlaneGeometry(30, 24);
     const texture = new THREE.TextureLoader().load(imgList[image]);
     console.log(imgList[image]);
     const material = new THREE.MeshBasicMaterial({
@@ -32,7 +36,7 @@ for (const image in imgList) {
         map: texture //add the texture image here
     });
     const plane = new THREE.Mesh(geometry, material);
-    plane.position.x = -20;
+    plane.position.x = -30;
     // texture.wrapS = THREE.RepeatWrapping;
     // texture.repeat.x = - 1;
     
@@ -53,7 +57,7 @@ scene.background = backTexture;
 function moveCamera() {
 
     const top = document.body.getBoundingClientRect().top;
-    camera.position.y = STARTY + top * 0.1
+    camera.position.y = STARTY + top * 0.2
     console.log(top);
 }
 
@@ -67,40 +71,42 @@ function resizeWindow() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     //Adjust for phone or desktop size
     if (window.innerWidth <= 600) {
-        camera.position.x = -20``
+        camera.position.x = -20
         for (const child in scene.children) {
-            scene.children[child].rotation.y = 0;
-            scene.children[child].position.y = child * -50;
+            scene.children[child].rotation.y = 22;
+            scene.children[child].position.y = child * -70;
+            scene.children[child].position.x = -20; 
+            scene.children[child].scale.set(1.2, 1.2)
         }
     } else {
         camera.position.x = 15
         for (const child in scene.children) {
             scene.children[child].rotation.y = 15 * (Math.PI / 180);
-            scene.children[child].position.y = (child * -26) + 10;
+            scene.children[child].position.y = (child * -36) + 8;
         }
     }
 }
 
-//Resize canvas on window resize
+// resize canvas on window resize
 window.addEventListener('resize', resizeWindow, false);
 
-//create the renderer and attach to the canvas
+// create the renderer and attach to the canvas
 const renderer = new THREE.WebGLRenderer(
     { canvas: document.querySelector('#bg') }
 );
 
-//Set initial canvas size
-resizeWindow();
 
-//Set renderer size and add it to the page
+resizeWindow()
+
+// set renderer size and add it to the page
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//animation loop (calls itself recursively)
-function animate(time) {
+// animation loop (calls itself recursively)
+function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
 
-//Start the animation
-animate();
+// start the animation
+animate()
